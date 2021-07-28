3f4917ed566cc6fe1c4513d0db73ceb29cf4cc5b
g33krock <dallas.lovell@gmail.com>
2021-07-27 20:18:51 -0700

    ormconfig.js

---
+++ server/ormconfig.js
@@ -0,0 +1,8 @@
+export default {
+    "type": "postgres",
+    "host": "db.qyctrtcwtwasdktftmuy.supabase.co",
+    "port": 5432,
+    "username": "postgres",
+    "password": process.env.TYPEORM_PASSWORD,
+    "database": "postgress"
+ }
