29a13f5544fd2a170650537a7b6fb2ad0eb1caf8
g33krock <dallas.lovell@gmail.com>
2021-07-27 20:25:53 -0700

    ormconfig.js 2

--- server/ormconfig.js
+++ server/ormconfig.js
@@ -1,8 +1,22 @@
 export default {
     "type": "postgres",
-    "host": "db.qyctrtcwtwasdktftmuy.supabase.co",
-    "port": 5432,
-    "username": "postgres",
-    "password": process.env.TYPEORM_PASSWORD,
-    "database": "postgress"
+	"host": "db.qyctrtcwtwasdktftmuy.supabase.co",
+	"port": 5432,
+	"username": "postgres",
+	"password": process.env.TYPEORM_PASSWORD,
+	"database": "postgres",
+	"synchronize": true,
+	"logging": false,
+	"entities": ["build/entity/*.js"],
+	"migrations": [
+		"src/migration/**/*.ts"
+	],
+	"subscribers": [
+		"src/subscriber/**/*.ts"
+	],
+	"cli": {
+		"entitiesDir": "src/entity",
+		"migrationsDir": "src/migration",
+		"subscribersDir": "src/subscriber"
+	}
  }
