1499e67618b4922c152adb90003c3421638ad13d
g33krock <dallas.lovell@gmail.com>
2021-08-11 19:13:17 -0700

    Fixed Transcript Issue

--- server/src/controller/TranscriptController.ts
+++ server/src/controller/TranscriptController.ts
@@ -4,11 +4,11 @@ import { Transcript } from "../entity/Transcript";
 export class TranscriptController {
 
     async all(request: Request, response: Response, next: NextFunction) {
-        return Transcript.find({ relations: ["student", "schedules", "teachers", "courses", "campus"] });
+        return Transcript.find({ relations: ["student", "schedules", "campus"] });
     }
 
     async one(request: Request, response: Response, next: NextFunction) {
-        return Transcript.findOne(request.params.id, { relations: ["student", "schedules", "teachers", "courses", "campus"] });
+        return Transcript.findOne(request.params.id, { relations: ["student", "schedules", "campus"] });
     }
 
 
