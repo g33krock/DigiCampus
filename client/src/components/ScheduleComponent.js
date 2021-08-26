59bb41a068611770395f3f01966c905dfa90cb67
g33krock <dallas.lovell@gmail.com>
2021-08-26 14:14:26 -0700

    Separate Elementary and High School Schedules

--- client/src/components/ScheduleComponent.js
+++ client/src/components/ScheduleComponent.js
@@ -425,6 +425,7 @@ export default class Schedule extends Component {
             <Col>
               <Label>Home Campus: {this.props.campus?.name}</Label>
             </Col>
+            <h1 id="headline">K-6</h1>
             <Table bordered hover size="sm">
               <thead class="shadow">
                 <tr id="scheduleHeader">
@@ -482,7 +483,119 @@ export default class Schedule extends Component {
                   .filter(
                     (cstudent) =>
                       cstudent.campuses.id === this.props?.campus?.id
-                  )
+                  ).filter(student => student.grade <= 6)
+                  .sort(function (a, b) {
+                    let x = a.firstName.toLowerCase();
+                    let y = b.firstName.toLowerCase();
+                    if (x < y) {
+                      return -1;
+                    }
+                    if (x > y) {
+                      return 1;
+                    }
+                    return 0;
+                  })
+                  .map((student) => (
+                    <tr>
+                      <th key={student.id}>
+                        {student.firstName} {student.lastName}
+                      </th>
+                      {student.schedules
+                        .sort((a, b) => a.period - b.period)
+                        .map((schedule) => (
+                          <td
+                            className={schedule.teacher?.firstName}
+                            id="schedItem"
+                          >
+                            <small>{schedule.period}</small>
+                            <br />
+                            <small>{schedule.course.name}</small>
+                            <br />
+                            <small>
+                              {schedule.teacher?.firstName}{" "}
+                              {schedule.teacher?.lastName}
+                            </small>{" "}
+                            <br />
+                            <ScheduleUpdater
+                              callback={() => this.getSchedules()}
+                              scheduleId={schedule.id}
+                              period={schedule.period}
+                              campus={schedule.campus}
+                              teacher={schedule.teacher}
+                              course={schedule.course}
+                              courses={this.state.courses}
+                              teachers={this.state.teachers}
+                            ></ScheduleUpdater>
+                            {/* <DeleteSchedule
+                              callback={() => this.getSchedules()}
+                              scheduleId={schedule.id}
+                              period={schedule.period}>
+                            </DeleteSchedule> */}
+                          </td>
+                        ))}
+                    </tr>
+                  ))}
+              </tbody>
+            </Table>
+            <h1 id="headline">7-12</h1>
+            <Table bordered hover size="sm">
+              <thead class="shadow">
+                <tr id="scheduleHeader">
+                  <th>
+                    <h2>Student</h2>
+                    <br /> <br />
+                  </th>
+                  <th>
+                    <br />
+                    <br />
+                    <br />
+                    <h2>Period 1</h2>
+                    <p>7:50-8:40</p>
+                  </th>
+                  <th>
+                    <h3>Period 2</h3>
+                    <p>8:40-9:30</p>
+                  </th>
+                  <th>
+                    <h3>Period 3</h3>
+                    <p>9:30-10:20</p>
+                  </th>
+                  <th>
+                    <h3>Period 4</h3>
+                    <p>10:20-11:10</p>
+                  </th>
+                  <th>
+                    <h3>Period 5</h3>
+                    <p>11:10-12:00</p>
+                  </th>
+                  <th>
+                    <h3>Period 6</h3>
+                    <p>12:00-12:50</p>
+                  </th>
+                  <th>
+                    <h3>Period 7</h3>
+                    <p>12:50-1:40</p>
+                  </th>
+                  <th>
+                    <h3>Period 8</h3>
+                    <p>1:40-2:30</p>
+                  </th>
+                  <th>
+                    <h3>Period 9</h3>
+                    <p>2:30-3:20</p>
+                  </th>
+                  <th>
+                    <h3>Period 10</h3>
+                    <p>3:20-4:10</p>
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.students
+                  .filter(
+                    (cstudent) =>
+                      cstudent.campuses.id === this.props?.campus?.id
+                  ).filter(student => student.grade >= 7)
                   .sort(function (a, b) {
                     let x = a.firstName.toLowerCase();
                     let y = b.firstName.toLowerCase();
@@ -547,6 +660,7 @@ export default class Schedule extends Component {
                 ))}
               </Input>
             </Col>
+            <h1 id="headline">K-6</h1>
             <Table bordered hover size="sm">
               <thead class="shadow">
                 <tr id="scheduleHeader">
@@ -604,7 +718,103 @@ export default class Schedule extends Component {
                   .filter(
                     (cstudent) =>
                       cstudent.campuses.id === this.state?.campus?.id
-                  )
+                  ).filter(student => student.grade <= 6)
+                  .sort(function (a, b) {
+                    let x = a.firstName.toLowerCase();
+                    let y = b.firstName.toLowerCase();
+                    if (x < y) {
+                      return -1;
+                    }
+                    if (x > y) {
+                      return 1;
+                    }
+                    return 0;
+                  })
+                  .map((student) => (
+                    <tr>
+                      <th key={student.id}>
+                        {student.firstName} {student.lastName}
+                      </th>
+                      {student.schedules
+                        .sort((a, b) => a.period - b.period)
+                        .map((schedule) => (
+                          <td
+                            className={schedule.teacher?.firstName}
+                            id="schedItem"
+                          >
+                            <small>{schedule.course.name}</small>
+                            <br />
+                            <small>
+                              {schedule.teacher?.firstName}{" "}
+                              {schedule.teacher?.lastName}
+                            </small>{" "}
+                            <br />
+                            <small>{schedule.teacher?.link} </small>{" "}
+                          </td>
+                        ))}
+                    </tr>
+                  ))}
+              </tbody>
+            </Table>
+            <h1 id="headline">7-12</h1>
+            <Table bordered hover size="sm">
+              <thead class="shadow">
+                <tr id="scheduleHeader">
+                  <th>
+                    <h2>Student</h2>
+                    <br /> <br />
+                  </th>
+                  <th>
+                    <br />
+                    <br />
+                    <br />
+                    <h2>Period 1</h2>
+                    <p>7:50-8:40</p>
+                  </th>
+                  <th>
+                    <h3>Period 2</h3>
+                    <p>8:40-9:30</p>
+                  </th>
+                  <th>
+                    <h3>Period 3</h3>
+                    <p>9:30-10:20</p>
+                  </th>
+                  <th>
+                    <h3>Period 4</h3>
+                    <p>10:20-11:10</p>
+                  </th>
+                  <th>
+                    <h3>Period 5</h3>
+                    <p>11:10-12:00</p>
+                  </th>
+                  <th>
+                    <h3>Period 6</h3>
+                    <p>12:00-12:50</p>
+                  </th>
+                  <th>
+                    <h3>Period 7</h3>
+                    <p>12:50-1:40</p>
+                  </th>
+                  <th>
+                    <h3>Period 8</h3>
+                    <p>1:40-2:30</p>
+                  </th>
+                  <th>
+                    <h3>Period 9</h3>
+                    <p>2:30-3:20</p>
+                  </th>
+                  <th>
+                    <h3>Period 10</h3>
+                    <p>3:20-4:10</p>
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.students
+                  .filter(
+                    (cstudent) =>
+                      cstudent.campuses.id === this.state?.campus?.id
+                  ).filter(student => student.grade >= 7)
                   .sort(function (a, b) {
                     let x = a.firstName.toLowerCase();
                     let y = b.firstName.toLowerCase();
