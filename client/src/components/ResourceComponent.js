9ac2089a91fba213d987ade14bc09fb6eaaf63f3
g33krock <dallas.lovell@gmail.com>
2021-09-01 20:31:27 -0700

    Added Donna's Low Cog Resource Link

--- client/src/components/ResourceComponent.js
+++ client/src/components/ResourceComponent.js
@@ -7,6 +7,7 @@ import {
   CardLink,
   Row,
   Col,
+  Container,
 } from "reactstrap";
 
 export default class Resource extends Component {
@@ -15,53 +16,76 @@ export default class Resource extends Component {
     this.state = {};
   }
 
-
   render() {
     return (
-      <Row>
-
-              <Col>
-                <Card body className="text-center">
-                  <CardTitle>STEAM Curriculum</CardTitle>
-                  <br />
-                  <CardBody>
-                    <CardText>Curriculum for Art/STEM, otherwise known as... STEAM</CardText>
-                  </CardBody>
-                  <CardLink href="https://drive.google.com/drive/folders/1q57URLhny9zFjPIaxXk2Y5XI7_hfhnwC?usp=sharing">STEAM Curriculum</CardLink>
-                </Card>
-              </Col>
-              <Col>
-                <Card body className="text-center">
-                  <CardTitle>Edgenuity</CardTitle>
-                  <br />
-                  <CardBody>
-                    <CardText>Wanna get to Edgenuity? Click here!</CardText>
-                  </CardBody>
-                  <CardLink href="https://www.edgenuity.com/">Edgenuity Link</CardLink>
-                </Card>
-              </Col>
-              <Col>
-                <Card body className="text-center">
-                  <CardTitle>IXL</CardTitle>
-                  <br />
-                  <CardBody>
-                    <CardText>Wanna get to IXL? Click here!</CardText>
-                  </CardBody>
-                  <CardLink href="https://www.ixl.com/">IXL Link</CardLink>
-                </Card>
-              </Col>
-              <Col>
-                <Card body className="text-center">
-                  <CardTitle>STAR Testing</CardTitle>
-                  <br />
-                  <CardBody>
-                    <CardText>Wanna get to STAR? Click here!</CardText>
-                  </CardBody>
-                  <CardLink href="https://global-zone50.renaissance-go.com/welcomeportal/7627521">STAR Link</CardLink>
-                </Card>
-              </Col>
-
-      </Row>
+      <Container>
+        <Row>
+          <Col>
+            <Card body className="text-center">
+              <CardTitle>STEAM Curriculum</CardTitle>
+              <br />
+              <CardBody>
+                <CardText>
+                  Curriculum for Art/STEM, otherwise known as... STEAM
+                </CardText>
+              </CardBody>
+              <CardLink href="https://drive.google.com/drive/folders/1q57URLhny9zFjPIaxXk2Y5XI7_hfhnwC?usp=sharing">
+                STEAM Curriculum
+              </CardLink>
+            </Card>
+          </Col>
+          <Col>
+            <Card body className="text-center">
+              <CardTitle>Low Cog Resources</CardTitle>
+              <br />
+              <CardBody>
+                <CardText>
+                  Resource Folder with games, worksheets, and activities for working with our low-cog students
+                </CardText>
+              </CardBody>
+              <CardLink href="https://drive.google.com/drive/folders/14WsaO2t1VDEu_idXVJ8MrFUNWTR6lZwc?usp=sharing">
+                Low Cog Resources
+              </CardLink>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Card body className="text-center">
+              <CardTitle>Edgenuity</CardTitle>
+              <br />
+              <CardBody>
+                <CardText>Wanna get to Edgenuity? Click here!</CardText>
+              </CardBody>
+              <CardLink href="https://www.edgenuity.com/">
+                Edgenuity Link
+              </CardLink>
+            </Card>
+          </Col>
+          <Col>
+            <Card body className="text-center">
+              <CardTitle>IXL</CardTitle>
+              <br />
+              <CardBody>
+                <CardText>Wanna get to IXL? Click here!</CardText>
+              </CardBody>
+              <CardLink href="https://www.ixl.com/">IXL Link</CardLink>
+            </Card>
+          </Col>
+          <Col>
+            <Card body className="text-center">
+              <CardTitle>STAR Testing</CardTitle>
+              <br />
+              <CardBody>
+                <CardText>Wanna get to STAR? Click here!</CardText>
+              </CardBody>
+              <CardLink href="https://global-zone50.renaissance-go.com/welcomeportal/7627521">
+                STAR Link
+              </CardLink>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
