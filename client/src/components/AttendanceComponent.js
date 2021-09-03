38ddde079d7846e9303b4a32f937f6f9c08d295f
g33krock <dallas.lovell@gmail.com>
2021-09-02 19:18:16 -0700

    Removed Method in Attendance

--- client/src/components/AttendanceComponent.js
+++ client/src/components/AttendanceComponent.js
@@ -51,9 +51,6 @@ export default class Attendance extends Component {
               <th>
                 <strong>Teacher</strong>
               </th>
-              <th>
-                <strong>Method</strong>
-              </th>
             </tr>
           </thead>
           <tbody>
@@ -82,9 +79,6 @@ export default class Attendance extends Component {
                 </td>
                 <td>
                   <small>{tracker?.teachers.firstName} {tracker?.teachers.lastName}</small>
-                </td>
-                <td>
-                  <small>{tracker?.method}</small>
                 </td>
                </tr>
             ))}
