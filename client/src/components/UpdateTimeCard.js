3479a0cf90a54dca7a216e5df9b2cf4f1aab3218
g33krock <dallas.lovell@gmail.com>
2021-09-07 08:23:13 -0700

    Timecard and New Marquee 2

--- client/src/components/UpdateTimeCard.js
+++ client/src/components/UpdateTimeCard.js
@@ -18,7 +18,7 @@ export class UpdateTimeCard extends Component {
       teacherID: this.props.teacher.id,
     };
     await teacherService.update(teacherObject);
-    await this.props.callback();
+    setTimeout(() => {  this.props.callback() }, 1000);
   }
 
   async clockOut() {
