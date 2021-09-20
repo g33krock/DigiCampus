76247a7fc49b99f0d4515e371d92f3b1b0cf2920
g33krock <dallas.lovell@gmail.com>
2021-09-20 12:55:23 -0700

    Progress Report 4

--- server/src/controller/StudentScheduleController.ts
+++ server/src/controller/StudentScheduleController.ts
@@ -4,7 +4,7 @@ import { Schedule } from "../entity/Schedule";
 export class StudentScheduleController {
 
 	async all(request: Request, response: Response, next: NextFunction) {
-		return Schedule.find({ relations: ["student", "course.id", "course.name"], where:{student:request.query.studentId} } );
+		return Schedule.find({ relations: ["student", "course"], where:{student:request.query.studentId} } );
 	}
 
 }
