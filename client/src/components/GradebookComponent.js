cf73599d4d6ac3f2c7a6bdf1db4c0cfafbfb3f24
g33krock <dallas.lovell@gmail.com>
2021-09-20 17:36:12 -0700

    Update Gradebook

--- client/src/components/GradebookComponent.js
+++ client/src/components/GradebookComponent.js
@@ -5,20 +5,24 @@ import {
   Modal,
   ModalBody,
   Card,
-  CardTitle,
   CardBody,
   Row,
   Col,
+  Form,
+  FormGroup,
+  Label,
+  Input,
 } from "reactstrap";
-import {GradeCalc} from "./GradeCalcComponent"
+import { GradeCalc } from "./GradeCalcComponent";
 import { fetcher } from "../services/fetcher";
+import { gradebookService } from "../services/gradebookService";
 
 export class ClassGrades extends Component {
   constructor(props) {
     super(props);
     this.state = {
       gradebooks: [],
-      gradebook:[],
+      gradebook: [],
       modal: false,
     };
   }
@@ -28,33 +32,59 @@ export class ClassGrades extends Component {
   }
 
   componentDidMount() {
-    fetcher(`${baseURL}/gradebooks`)
-      .then((response) => response.json())
-      .then((data) => {
-        this.setState({
-          gradebooks: data,
-        });
+    this.getGradebook();
+  }
+
+  getGradebook() {
+    fetcher(`${baseURL}/studentgradebooks?studentsId=${this.props.student.id}`)
+    .then((response) => response.json())
+    .then((data) => {
+      this.setState({
+        gradebooks: data,
       });
+    });
+  }
+
+  async updateGradebook(i) {
+    const gradebookObject = {
+      id: i.id,
+      pointsAvailable: parseInt(
+        document.getElementById("pointsAvailable" + i.id).value
+      ),
+      pointsEarned: parseInt(
+        document.getElementById("pointsEarned" + i.id).value
+      ),
+      name: document.getElementById("name" + i.id).value,
+    };
+    await gradebookService.update(gradebookObject);
+  }
+
+  async updateGradebookNinja() {
+    this.state.gradebooks.forEach((scheduleQuestion) =>
+      this.updateGradebook(scheduleQuestion)
+    );
+    setTimeout(() => {  this.getGradebook() }, 2000);
   }
 
   async whatsGoingOnHere() {
-    await this.setState({ gradebook:this.gradebooks })
-    await console.log(this.state.gradebook)
+    await this.setState({ gradebook: this.gradebooks });
+    await console.log(this.state.gradebook);
   }
 
-  
-
   render() {
-    const gradebook = this.state.gradebook
     const gradebooks = this.state.gradebooks
       .filter((gradebook) => gradebook.courses.id === this.props.course.id)
       .filter((gradebook) => gradebook.students.id === this.props.student.id)
-      .filter((gradebook) => gradebook.pointsAvailable > 0 );
+      .filter((gradebook) => gradebook.pointsAvailable > 0);
     console.log(gradebooks);
-    const reducer = (previousValue, currentValue) => previousValue + currentValue;
     return (
       <div>
-        <Button color="link" onClick={() => {this.setState({ modal: true })}}>
+        <Button
+          color="link"
+          onClick={() => {
+            this.setState({ modal: true });
+          }}
+        >
           Grades
         </Button>
         <Modal isOpen={this.state.modal} toggle={this.toggle}>
@@ -67,23 +97,74 @@ export class ClassGrades extends Component {
                 </h3>
               </Col>
               <Col>
-              <GradeCalc
-              gradebook={gradebooks} />
+                <GradeCalc gradebook={gradebooks} />
               </Col>
             </Row>
             {gradebooks.map((gradebook) => (
               <Card>
                 <CardBody>
-                  <div>
+                  <Form className="fancy-cursor">
+                    <FormGroup id="trackerBox">
+                      <Label for="name">
+                        <strong>Assignment Name</strong>
+                      </Label>
+                      <Input
+                        type="text"
+                        name="name"
+                        id={`name${gradebook.id}`}
+                        className="fancy-cursor"
+                        defaultValue={gradebook.name}
+                      ></Input>
+                    </FormGroup>
+                    <FormGroup id="trackerBox">
+                      <Label for="grade">
+                        <strong>Grade</strong>
+                      </Label>
+                      <Row>
+                        <Col xs="3">
+                          <Input
+                            type="number"
+                            name="pointsEarned"
+                            id={`pointsEarned${gradebook.id}`}
+                            className="fancy-cursor"
+                            defaultValue={gradebook.pointsEarned}
+                          ></Input>
+                        </Col>
+                        <Col xs="1">
+                          <strong>/</strong>
+                        </Col>
+                        <Col xs="3">
+                          <Input
+                            type="number"
+                            name="pointsAvailable"
+                            id={`pointsAvailable${gradebook.id}`}
+                            className="fancy-cursor"
+                            defaultValue={gradebook.pointsAvailable}
+                          ></Input>
+                        </Col>
+                        <Col />
+                      </Row>
+                    </FormGroup>
+                  </Form>
+                  {/* <div>
                     <p>Assignment: {gradebook.name}</p>
                     <p>
                       Points: {gradebook.pointsEarned}/
                       {gradebook.pointsAvailable}
                     </p>
-                  </div>
+                  </div> */}
                 </CardBody>
               </Card>
             ))}
+            <Button
+              color="primary"
+              onClick={() => {
+                this.setState({ modal: false });
+                this.updateGradebookNinja();
+              }}
+            >
+              Submit Changes
+            </Button>
 
             <Button
               color="danger"
