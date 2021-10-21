75768ebed44ed7f32328c25982c5e33cf1885355
g33krock <dallas.lovell@gmail.com>
2021-10-21 11:32:25 -0700

    Attendance by District in Billing

--- client/src/components/BillingComponent.js
+++ client/src/components/BillingComponent.js
@@ -7,12 +7,19 @@ import {
   TabContent,
   Table,
   TabPane,
+  Col,
+  Form,
+  Row,
+  Label,
+  Input,
+  Button
 } from "reactstrap";
 import { baseURL } from "../baseURL";
 import { fetcher } from "../services/fetcher";
 import classnames from "classnames";
 import { isNull } from "lodash";
 import { studentService } from "../services/studentService";
+import { attendanceService } from "../services/attendanceService";
 
 export default class Billing extends Component {
   constructor(props) {
@@ -24,10 +31,17 @@ export default class Billing extends Component {
     this.state = {
       students: [],
       districts: [],
+      district: null,
+      startDate: new Date(new Date().getTime() - 14 * 24 * 60 * 60 * 1000)
+      .toISOString()
+      .slice(0, 10),
+    endDate: new Date().toISOString().slice(0, 10),
+    attendance: [],
     };
   }
 
   async componentDidMount() {
+    this.getAttendance();
     const students = await studentService.all();
     this.setState({ students });
     fetcher(`${baseURL}/districts`)
@@ -37,6 +51,33 @@ export default class Billing extends Component {
         districts: data,
       });
     });
+  }
+
+  getAttendance() {
+    attendanceService.all().then((attendance) => {
+      this.setState({
+        attendance,
+      });
+      console.log(this.state.attendance);
+    });
+  }
+
+  onChange = (e) => {
+    const districtId = Number(e.target.value);
+    const district = this.state.districts.find((district) => district.id === districtId);
+    this.setState({ district });
+    console.log(district);
+    console.log(e.target.value);
+    this.getAttendance();
+  };
+
+  async saveTheDate() {
+    const date = await this.setState({
+      startDate: document.getElementById("startDate").value,
+      endDate: document.getElementById("endDate").value,
+    });
+    console.log(date);
+    console.log(`Start: ${this.state.startDate}  End: ${this.state.endDate}`);
   }
 
   toggle(tab) {
@@ -64,6 +105,17 @@ export default class Billing extends Component {
       }
   }
 
+  attendanceSwitch(randomInt) {
+    switch (randomInt) {
+      case 0:
+        return "Absent";
+      case 1:
+        return "Present";
+      default:
+        return "Incorporeal Being";
+    }
+  }
+
   render() {
     return (
       <div>
@@ -86,6 +138,16 @@ export default class Billing extends Component {
               }}
             >
               District
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              className={classnames({ active: this.state.activeTab === "3" })}
+              onClick={() => {
+                this.toggle("3");
+              }}
+            >
+              Attendance
             </NavLink>
           </NavItem>
         </Nav>
@@ -250,6 +312,122 @@ export default class Billing extends Component {
             </Container>
             )}
             </TabPane>
+            <TabPane tabId="3">
+            <Col md="8">
+              <Form>
+                <Row>
+                  <Col md="3">
+                    <Label for="startDate">
+                      <small>Start Date</small>
+                    </Label>
+                    <Input
+                      type="date"
+                      name="startDate"
+                      id="startDate"
+                      placeholder="Start"
+                    />
+                  </Col>
+                  <Col md="3">
+                    <Label for="endDate">
+                      <small>End Date</small>
+                    </Label>
+                    <Input
+                      type="date"
+                      name="endDate"
+                      id="endDate"
+                      placeholder="End"
+                    />
+                  </Col>
+                  <Col md="2">
+                    <br />
+                    <Button
+                      color="primary"
+                      onClick={() => {
+                        this.saveTheDate();
+                      }}
+                    >
+                      Update
+                    </Button>
+                  </Col>
+                </Row>
+              </Form>
+            </Col>
+            <Col sm={3}>
+              <Label>Select District: </Label>
+              <Input type="select" id="selectCampus" onChange={this.onChange}>
+                <option></option>
+                {this.state.districts.map((district) => (
+                  <option value={district.id}>{district.name}</option>
+                ))}
+              </Input>
+            </Col>
+            <h1 id="headline">{this.state?.district?.name}</h1>
+            <Table bordered hover size="sm" className="tight">
+              <thead class="shadow" id="scheduleHeader">
+                <tr>
+                  <th>
+                    <h1>Student</h1>
+                    <br /> <br /> <br />
+                  </th>
+                  {this.state.attendance
+                    .filter(
+                      (day) =>
+                        day.date >= this.state.startDate &&
+                        day.date <= this.state.endDate
+                    )
+                    .map((day) => (
+                      <th>{day.date}</th>
+                    ))}
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.students
+                  .filter(
+                    (cstudent) =>
+                      cstudent.district?.id === this.state?.district?.id
+                  )
+                  .sort(function (a, b) {
+                    let x = a.firstName.toLowerCase();
+                    let y = b.firstName.toLowerCase();
+                    if (x < y) {
+                      return -1;
+                    }
+                    if (x > y) {
+                      return 1;
+                    }
+                    return 0;
+                  })
+                  .map((student) => (
+                    <tr>
+                      <th key={student.id}>
+                        {student.firstName} {student.lastName}
+                      </th>
+                      {this.state.attendance
+                        .filter(
+                          (day) =>
+                            day.date >= this.state.startDate &&
+                            day.date <= this.state.endDate
+                        )
+                        .map((day) => (
+                          <td
+                            className={this.attendanceSwitch(
+                              day.student.filter(
+                                (here) => here.id === student.id
+                              ).length
+                            )}
+                          >
+                            {this.attendanceSwitch(
+                              day.student.filter(
+                                (here) => here.id === student.id
+                              ).length
+                            )}
+                          </td>
+                        ))}
+                    </tr>
+                  ))}
+              </tbody>
+            </Table>
+          </TabPane>
           </TabContent>
         </div>
       </div>
