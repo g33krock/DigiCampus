3a0b69ed0c53af91babd1ab46ab9e80692052496
eeevvv <evan.w.and@gmail.com>
2021-12-14 13:46:14 -0500

    FIX: add percentages to letter grades

--- client/src/components/FinalGrades.js
+++ client/src/components/FinalGrades.js
@@ -229,19 +229,19 @@ const Input = ({ handleChange, id, scheduleObj }) => {
       // return an input field with letter grades
       <select defaultValue="" onChange={(e) => handleChange(scheduleObj, id, e)}>
         <option hidden value="">Select a grade</option>
-        <option value="A+">A+</option>
-        <option value="A">A</option>
-        <option value="A-">A-</option>
-        <option value="B+">B+</option>
-        <option value="B">B</option>
-        <option value="B-">B-</option>
-        <option value="C+">C+</option>
-        <option value="C">C</option>
-        <option value="C-">C-</option>
-        <option value="D+">D+</option>
-        <option value="D">D</option>
-        <option value="D-">D-</option>
-        <option value="F">F</option>
+        <option value="A+">A+ (97-100)</option>
+        <option value="A">A (93-96)</option>
+        <option value="A-">A- (90-92)</option>
+        <option value="B+">B+ (87-89)</option>
+        <option value="B">B (83-86)</option>
+        <option value="B-">B- (80-82)</option>
+        <option value="C+">C+ (77-79)</option>
+        <option value="C">C (73-76)</option>
+        <option value="C-">C- (70-72)</option>
+        <option value="D+">D+ (67-69)</option>
+        <option value="D">D (63-66)</option>
+        <option value="D-">D- (60-62)</option>
+        <option value="F">F (below 60)</option>
         <option value="In Progress">In Progress</option>
       </select>
     );
