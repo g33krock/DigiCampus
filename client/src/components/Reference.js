7fe38c96d5533248eaa1628d4f620394d0dcc203
eeevvv <evan.w.and@gmail.com>
2022-01-14 14:29:17 -0500

    finish reference page and clean up tracker

--- client/src/components/Reference.js
+++ client/src/components/Reference.js
@@ -2,12 +2,19 @@ import styles from '../styles/Reference.module.css'
 
 
 export default function Reference() {
-  const comprehension = [
+  const engagement = [
     "Disengaged: diverted attention/no commitment/avoids learning",
     "Retreatism: no attention/no commitment/little inquiry, interest or collaboration in learning",
     "Ritual Compliance: low attention/low commitment/minimal effort to avoid negative consequences, no self direction or motivation, minimal inquiry",
     "Strategic Compliance: high attention/low commitment/minimal effort, focus and task completion to meet learning",
     "Engagement: high attention/high commitment/self-directed learning, persistent sustained inquiry, self-directed learning, highly motivated and engaged",
+  ]
+  const comprehension = [
+    "No Demonstration: of concept mastery/No work samples",
+    "Beginning: Demonstrates little comprehension of concept",
+    "Developing: Demonstrates incomplete and/or partial comprehension of concepts with some errors",
+    "Proficient: Demonstrates considerable comprehension of concept",
+    "Mastery: Demonstrates complete and thorough comprehension of concept",
   ]
 
   return (
@@ -16,19 +23,28 @@ export default function Reference() {
       <h2>Tracking</h2>
       <p>Below is some information to which you can refer while filling out student tracking.</p>
       <div className={styles.section}>
+        <h3>Scale of Engagement Scores</h3>
+        {
+          engagement.map((description, idx) => <Box description={description} value={idx + 1} />)
+        }
+      </div>
+      <div className={styles.section}>
         <h3>Scale of Comprehension/Mastery Scores</h3>
         {
-          comprehension.map((description, idx) => {
-            return (
-              <div className={styles.box}>
-                <div className={styles.row}>
-                  <div className={styles.cell}>{idx + 1}</div>
-                  <div className={styles.cell}>{description}</div>
-                </div>
-              </div>
-            )
-          })
+          comprehension.map((description, idx) => <Box description={description} value={idx} />)
         }
+      </div>
+    </div>
+  )
+}
+
+function Box(props) {
+  const { description, value } = props
+  return (
+    <div className={styles.box}>
+      <div className={styles.row}>
+        <div className={styles.cell}><strong>{value}</strong></div>
+        <div className={styles.cell}>{description}</div>
       </div>
     </div>
   )
